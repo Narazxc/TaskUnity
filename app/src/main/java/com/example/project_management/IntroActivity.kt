5b70d5fe5c136390451b0d9f08d322acb681e03b
TangLymeng <tanglymeng2@gmail.com>
2023-11-15 20:06:38 +0700

    Setup Signup activity

--- app/src/main/java/com/example/project_management/IntroActivity.kt
+++ app/src/main/java/com/example/project_management/IntroActivity.kt
@@ -1,11 +1,20 @@
 package com.example.project_management
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project_management.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignUpIntro.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, SignUpActivity::class.java))
+        }
     }
 }
